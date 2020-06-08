a7d88673e9a479901f7bbe055146222e7e896064
lxk <1271215069@qq.com>
2020-06-08 23:34:03 +0800

    项目提交

---
+++ lib/data_type.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart'; //  引入flutter基础样式
+class dataType extends StatefulWidget {
+  @override
+  _dataTypeState createState() => _dataTypeState();
+}
+
+class _dataTypeState extends State<dataType> {
+  @override
+  Widget build(BuildContext context) {
+    _numType();
+    return Container();
+  }
+  void _numType () {
+
+  }
+}
